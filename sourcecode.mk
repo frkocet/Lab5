ca31abec3ef1461847e013d010200bbe382568eb
frkocet <145404908+frkocet@users.noreply.github.com>
2024-03-04 15:30:34 -0800

    first commit

---
+++ sourcecode.mk
@@ -0,0 +1,30 @@
+SHELL=cmd
+CC=c51
+COMPORT = $(shell type COMPORT.inc)
+OBJS=sourcecode.obj
+
+sourcecode.hex: $(OBJS)
+	$(CC) $(OBJS)
+	@echo Done!
+	
+sourcecode.obj: sourcecode.c
+	$(CC) -c sourcecode.c
+
+clean:
+	@del $(OBJS) *.asm *.lkr *.lst *.map *.hex *.map 2> nul
+
+LoadFlash:
+	@Taskkill /IM putty.exe /F 2>NUL | wait 500
+	EFM8_prog.exe -ft230 -r sourcecode.hex
+	cmd /c start putty -serial $(COMPORT) -sercfg 115200,8,n,1,N
+
+putty:
+	@Taskkill /IM putty.exe /F 2>NUL | wait 500
+	cmd /c start putty -serial $(COMPORT) -sercfg 115200,8,n,1,N
+
+Dummy: sourcecode.hex sourcecode.Map
+	@echo Nothing to see here!
+	
+explorer:
+	cmd /c start explorer .
+		
