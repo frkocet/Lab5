4ce9444a7b622dfe484cfb174c40bb2ca2676873
frkocet <145404908+frkocet@users.noreply.github.com>
2024-03-05 21:01:05 -0800

    balls

--- sourcecode.mk
+++ sourcecode.mk
@@ -22,7 +22,7 @@ putty:
 	@Taskkill /IM putty.exe /F 2>NUL | wait 500
 	cmd /c start putty -serial $(COMPORT) -sercfg 115200,8,n,1,N
 
-Build&Run: $(OBJS)
+make: $(OBJS)
 	$(CC) $(OBJS)
 	@Taskkill /IM putty.exe /F 2>NUL | wait 500
 	EFM8_prog.exe -ft230 -r sourcecode.hex
